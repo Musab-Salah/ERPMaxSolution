758940324014ac19ea561882c701ef702e6f97e8
Musab Salah <musabsalah1998@gmail.com>
2023-06-19 19:52:06 +0300

    Fix Performance Issue

--- src/business_logic/report/report_manager.py
+++ src/business_logic/report/report_manager.py
@@ -3,32 +3,37 @@ class ReportManager:
     def get_product_balance():
         locations = Location.query.all()
         products = Product.query.all()
+        movements = ProductMovement.query.filter(
+            (ProductMovement.to_location_id.in_([location.location_id for location in locations])) |
+            (ProductMovement.from_location_id.in_([location.location_id for location in locations]))
+        ).all()
+
         balance = {}
 
         for location in locations:
             products_in_location = []
+            
             for product in products:
-                movements_in = ProductMovement.query.filter_by(to_location_id=location.location_id, product_id=product.product_id).all()
-                total_in = 0
-                for movement in movements_in:
-                    total_in += movement.qty
-                
-                movements_out = ProductMovement.query.filter_by(from_location_id=location.location_id, product_id=product.product_id).all()
-                total_out = 0
-                for movement in movements_out:
-                    total_out += movement.qty
+                total_in = sum(movement.qty for movement in movements if
+                            movement.to_location_id == location.location_id and movement.product_id == product.product_id)
+                total_out = sum(movement.qty for movement in movements if
+                                movement.from_location_id == location.location_id and movement.product_id == product.product_id)
 
                 qty = total_in - total_out
-                if qty >= 0 and (movements_out or movements_in):
+                
+                if qty >= 0 and (total_in > 0 or total_out > 0):
                     products_in_location.append({
                         'Product ID': product.product_id,
                         'Product Name': product.product_name,
                         'Qty': qty
-                    })        
+                    })
+            
             if products_in_location:
                 balance[location.location_id] = {
                     'Location ID': location.location_id,
                     'Location Name': location.location_name,
                     'Products': products_in_location
                 }
+        
         return balance
+
